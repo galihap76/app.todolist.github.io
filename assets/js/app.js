6181167e26abad50604c1c0ef81f18426f713269
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2024-07-02 22:19:03 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/assets/js/service-worker.js")
+      .register("service-worker.js")
       .then((registration) => {
         console.log("ServiceWorker registered with scope:", registration.scope);
       })
